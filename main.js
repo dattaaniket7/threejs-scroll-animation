9d037687200bc11a716a5cec1ea4a3ee401982c1
Aniket Datta <datta.aniket7@gmail.com>
2023-08-27 01:02:06 +0530

    renderer

--- main.js
+++ main.js
@@ -10,3 +10,13 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
