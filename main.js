5a6bd6797e9826b153f717a629e414dc627bfe1f
Aniket Datta <datta.aniket7@gmail.com>
2023-08-27 03:06:09 +0530

    changes

--- main.js
+++ main.js
@@ -70,14 +70,14 @@ scene.background = spaceTexture;
 
 // Avatar
 
-const jeffTexture = new THREE.TextureLoader().load("jeff.png");
+const aniketTexture = new THREE.TextureLoader().load("aniket.jpg");
 
-const jeff = new THREE.Mesh(
+const aniket = new THREE.Mesh(
   new THREE.BoxGeometry(3, 3, 3),
-  new THREE.MeshBasicMaterial({ map: jeffTexture })
+  new THREE.MeshBasicMaterial({ map: aniketTexture })
 );
 
-scene.add(jeff);
+scene.add(aniket);
 
 // Moon
 
@@ -97,8 +97,8 @@ scene.add(moon);
 moon.position.z = 30;
 moon.position.setX(-10);
 
-jeff.position.z = -5;
-jeff.position.x = 2;
+aniket.position.z = -5;
+aniket.position.x = 2;
 
 // Scroll Animation
 
@@ -108,8 +108,8 @@ function moveCamera() {
   moon.rotation.y += 0.075;
   moon.rotation.z += 0.05;
 
-  jeff.rotation.y += 0.01;
-  jeff.rotation.z += 0.01;
+  aniket.rotation.y += 0.01;
+  aniket.rotation.z += 0.01;
 
   camera.position.z = t * -0.01;
   camera.position.x = t * -0.0002;
